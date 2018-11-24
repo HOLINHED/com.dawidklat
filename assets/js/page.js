f951068f6fd6bd09ebf5371e8eda568456b84e39
Holinhed <synergycocyt@gmail.com>
2018-11-24 17:07:10 -0500

    most

--- assets/js/page.js
+++ assets/js/page.js
@@ -4,7 +4,7 @@ fetch("/links.php")
    })
    .then(data => {
       const titles = data.titles;
-      const index = Math.floor( Math.random() * titles.length);
+      const index = Math.floor(Math.random() * titles.length);
       document.title = titles[index];
       
       makePage(data.links);   
@@ -15,6 +15,41 @@ fetch("/links.php")
 
 function makePage(links){
    const page = document.querySelector('#container');
+   page.innerHTML = "";
 
-   console.log(links);
+   const table = document.createElement("table");
+
+   for (link of links){
+
+      const row = document.createElement("tr");
+
+      const linkCol = document.createElement("td");
+      const links = document.createElement("a");
+      links.innerHTML = link.name;
+      links.setAttribute("href", link.link);
+      links.setAttribute("class", "link");
+      linkCol.appendChild(links);
+
+      row.appendChild(linkCol);
+
+      const dateCol = document.createElement("td");
+      const date = document.createElement("text");
+      date.setAttribute("class", "text");
+      date.innerHTML = `${link.date}`
+      dateCol.appendChild(date);
+
+      row.appendChild(dateCol);
+
+      const descCol = document.createElement("td");
+      const desc = document.createElement("text");
+      desc.setAttribute("class", "text");
+      desc.innerHTML = `${link.desc}`;
+      descCol.appendChild(desc);
+
+      row.appendChild(descCol);
+
+      table.appendChild(row);
+   }
+
+   page.appendChild(table);
 }
--- links.php
+++ links.php
@@ -2,7 +2,7 @@
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET'){
    //GET
-   $json = file_get_contents('./assets/json/links.json');
+   $json = file_get_contents('./assets/json/data.json');
    $data = json_decode($json, true);
    header('Content-Type: application/json');
    echo json_encode($data);
